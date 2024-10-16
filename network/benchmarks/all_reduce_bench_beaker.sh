3bf3d6b3aa5cb461b13f0d8a9ad57c6ec9920792
Dirk Groeneveld <dirkg@allenai.org>
2024-10-16 15:57:35 -0700

    Have to download the code now

--- network/benchmarks/all_reduce_bench_beaker.sh
+++ network/benchmarks/all_reduce_bench_beaker.sh
@@ -11,6 +11,9 @@ export NCCL_DEBUG=INFO
 export NCCL_IB_HCA="^=mlx5_bond_0"
 export NCCL_SOCKET_IFNAME=ib
 export TORCH_DIST_INIT_BARRIER=1
+
+curl -O https://raw.githubusercontent.com/dirkgr/ml-engineering/refs/heads/Beaker/network/benchmarks/all_reduce_bench.py
+
 torchrun \
 	--nnodes "${NUM_NODES}:${NUM_NODES}" \
 	--nproc-per-node 8 \
@@ -19,4 +22,4 @@ torchrun \
 	--rdzv_endpoint "${BEAKER_LEADER_REPLICA_HOSTNAME}:29400" \
 	--node_rank "${BEAKER_REPLICA_RANK}" \
 	--rdzv_conf 'read_timeout=420' \
-	network/benchmarks/all_reduce_bench.py
+	all_reduce_bench.py
